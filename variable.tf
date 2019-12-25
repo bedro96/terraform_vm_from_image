3e758c5c244b0bb451e12d30a5a264b36fee3a6f
Kunho Ko <bedro96@naver.com>
2019-12-25 19:08:26 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,10 +2,6 @@ variable "resourcegroup_name" {
   description = "The name of resource group"
   default = "terraformrg"
 }
-variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-  default = "terraformvmss"
-}
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "southeastasia"
@@ -13,12 +9,12 @@ variable "location" {
 variable "ssh_username" {
   description = "ssh username."
 }
+variable "ssh_password" {
+  description = "ssh password."
+}
 variable "ssh_port" {
   description = "ssh Port."
   default = "22"
-}
-variable "ssh_password" {
-  description = "ssh password."
 }
 variable "vm_size" {
   description = "The vm SKU"
